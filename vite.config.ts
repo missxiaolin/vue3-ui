0d9f93971fc4a09b967f5292c89d21c4fa890d16
xiaolin <462441355@qq.com>
2022-01-24 16:56:05 +0800

    * 引入scss

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,22 @@ export default defineConfig({
             { find: '@xl-ui', replacement: resolve(__dirname, './src') }
         ]
     },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                // example : additionalData: `@import "./src/design/styles/variables";`
+                // dont need include file extend .scss
+                // additionalData: `@import "@/packages/styles/variables.scss";@import "@/sites/assets/styles/variables.scss";`
+              }
+        },
+        postcss: {
+            plugins: [
+                require('autoprefixer')({
+                    overrideBrowserslist: ['> 0.5%', 'last 2 versions', 'ie > 11', 'iOS >= 10', 'Android >= 5']
+                })
+            ]
+        }
+    },
     plugins: [
         vitePluginVuedoc({}),
         vue({
