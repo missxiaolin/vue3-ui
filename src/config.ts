531279974020641ec5f312c64c03d43769baf6dc
xiaolin <462441355@qq.com>
2022-02-11 17:14:43 +0800

    * header 部分

---
+++ src/config.ts
@@ -0,0 +1,22 @@
+const config: any = {
+    "versions": [
+        {
+            "name": "1.x",
+            "link": "/1x/"
+        }
+    ],
+    "header": [
+        {
+            "name": "intro,theme,start",
+            "cName": "指南",
+            "path": "/intro"
+        },
+        {
+            "name": "component",
+            "cName": "组件",
+            "path": "/layout"
+        }
+    ],
+}
+
+export default config
--- src/sites/pc/components/header.vue
+++ src/sites/pc/components/header.vue
@@ -1,33 +1,59 @@
 <template>
-  <div class="doc-header">
-    <div class="header-logo">
-      <a class="logo-link" href="/" @click="toHome"></a>
-      <span class="logo-border"></span>
-      <span class="version">v{{ version }}</span>
+  <div class="doc-header" :class="themeName()">
+    <div class="header-nav">
+      <Search />
+      <div class="nav-box">
+        <ul class="nav-list">
+          <li class="nav-item" v-for="item in header" :key="item.name">
+            <a :href="item.path">
+              {{ item.cName }}
+            </a>
+          </li>
+          <li class="nav-item">
+            <div class="header-select-box">
+              <div class="header-select-hd">1.0.0<i class=""></i></div>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import { defineComponent, ref, computed } from 'vue';
 import { RefData } from '../../assets/utils/ref'
+import config from '../../../config';
+import Search from './Search.vue';
 
 export default defineComponent({
   name: 'doc-header',
-
+  components: {
+    Search
+  },
   setup() {
     let version = ref<string>('1.0.0');
+    let header = ref<any>(config.header)
+    const themeName = computed(() => {
+      return function () {
+        return `doc-header-${RefData.getInstance().themeColor.value}`;
+      };
+    });
 
     const toHome = () => {
       RefData.getInstance().currentRoute.value = '/';
     };
+
     return {
+      header,
       version,
-      toHome
+      toHome,
+      themeName
     };
   }
 });
 </script>
 
 <style lang="scss">
+@import '../../assets/style/header.scss';
 </style>
