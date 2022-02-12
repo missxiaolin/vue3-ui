a47a91ce2fe09d68867761c0cf7b7fe86c111661
xiaolin <462441355@qq.com>
2022-02-12 22:03:38 +0800

    * 初始化ui 架构

--- src/sites/pc/main.ts
+++ src/sites/pc/main.ts
@@ -1,7 +1,13 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router/index';
+import '../assets/style/index.scss'
+
+import UI from '../../packages/ui';
+import '../../packages/styles/index.scss'
+
 
 const app = createApp(App);
+app.use(UI);
 app.use(router);
 app.mount('#app');
