f6da92fddee47fa7a8a5da6abe2a6f9c30e01486
462441355@qq.com <462441355@qq.com>
2023-04-26 20:03:40 +0800

    * 初始化样式

--- src/sites/pc/view/main.vue
+++ src/sites/pc/view/main.vue
@@ -11,6 +11,7 @@ import { defineComponent } from 'vue';
 import Header from '@/sites/pc/components/header.vue';
 import Footer from '@/sites/pc/components/footer.vue';
 
+
 export default defineComponent({
   name: 'Main',
   components: {
