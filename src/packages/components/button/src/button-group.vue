b89fabf4e1ed0bd04111dd1ca50de933740d82ce
462441355@qq.com <462441355@qq.com>
2023-05-09 09:50:51 +0800

    * 初始化组合btn

--- src/packages/components/button/src/button-group.vue
+++ src/packages/components/button/src/button-group.vue
@@ -1,3 +1,21 @@
 <template>
-    <div>2</div>
+  <div>
+    {{ name }}
+    <slot></slot>
+  </div>
 </template>
+
+<script lang="ts">
+import { defineComponent, provide, reactive, toRef, toRefs } from 'vue';
+import { buttonGroupProps } from './button-group';
+
+import createComponent from '../../../utils/create';
+const { componentName, useGlobalConfig, create } = createComponent('ButtonGroup');
+export default create({
+  props: buttonGroupProps,
+
+  setup(props) {
+    
+  }
+});
+</script>
