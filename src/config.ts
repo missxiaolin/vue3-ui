6751921db76ddcd1def9d73e00a1e98688d30e6d
462441355@qq.com <462441355@qq.com>
2023-05-14 09:34:31 +0800

    * update

--- src/config.ts
+++ src/config.ts
@@ -89,17 +89,7 @@ const config: any = {
             "show": true,
             "author": "lin",
             "progress": "100%"
-          }, 
-          // {
-          //   "name": "Gradient",
-          //   "type": "component",
-          //   "cName": "渐变文字",
-          //   "desc": "渐变文字",
-          //   "show": true,
-          //   "author": "",
-          //   "progress": "0%"
-          // },
-           {
+          }, {
             "name": "Icon",
             "type": "component",
             "cName": "图标",
