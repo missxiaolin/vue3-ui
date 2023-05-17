499335af64e71f4c77b729d545aca22d5eed794a
462441355@qq.com <462441355@qq.com>
2023-05-17 10:15:20 +0800

    * 锚点组件完成

--- src/packages/components/anchor/docs/doc.md
+++ src/packages/components/anchor/docs/doc.md
@@ -9,15 +9,17 @@
 ```vue demo
 <template>
     <div style="background: #F5F6F9;padding: 20px;">
-        <l-anchor-link href="#anchor-1231锚点" title="锚点">
-            <l-anchor-link href="#介绍" title="介绍" />
-            <l-anchor-link href="#基础用法" title="基础用法" />
-        </l-anchor-link>
-        <l-anchor-link href="#api" title="API">
-            <l-anchor-link href="#anchor-属性" title="anchor-属性"></l-anchor-link>
-            <l-anchor-link href="#anchor抛出事件" title="anchor抛出事件"></l-anchor-link>
-            <l-anchor-link href="#link-props" title="anchor Link Props"></l-anchor-link>
-        </l-anchor-link>
+        <l-anchor :affix="true" :offsetTop="50" :targetOffset="20" defaultAnchor="anchor-1231锚点"  wrapperClass="my-wrapperClass">
+            <l-anchor-link href="#anchor-1231锚点" title="锚点">
+                <l-anchor-link href="#介绍" title="介绍" />
+                <l-anchor-link href="#基础用法" title="基础用法" />
+            </l-anchor-link>
+            <l-anchor-link href="#api" title="API">
+                <l-anchor-link href="#anchor-属性" title="anchor-属性"></l-anchor-link>
+                <l-anchor-link href="#anchor抛出事件" title="anchor抛出事件"></l-anchor-link>
+                <l-anchor-link href="#link-props" title="anchor Link Props"></l-anchor-link>
+            </l-anchor-link>
+        </l-anchor>
     </div>
 </template>
 ```
--- src/packages/components/anchor/src/anchor.vue
+++ src/packages/components/anchor/src/anchor.vue
@@ -1,36 +1,273 @@
 <template>
-    <div>
-        锚点
+  <div ref="root" v-if="!affix" :class="[ns.b(), wrapperClass]" :style="wrapperStyle" :ref="anchorRef">
+    <transition
+      :name="`${ns.b()}-fade`"
+      @after-enter="afterEnter"
+      @after-leave="afterLeave"
+      @before-leave="beforeLeave"
+    >
+      <div :class="[`${ns.b()}_content`, anchorClass, 'ltr']" v-show="visible">
+        <div :class="[`${ns.b()}-ink`]">
+          <span :ref="inkNodeRef" />
+        </div>
+        <slot></slot>
+      </div>
+    </transition>
+    <div :class="`${ns.b()}__bar`" @click="visible = !visible">
+      <e-icon icon="e-icon-two-arrow-right"></e-icon>
     </div>
+  </div>
+  <l-sticky v-else :offset="props.offsetTop">
+    <div ref="root" :class="[ns.b(), wrapperClass]" :style="wrapperStyle" :ref="anchorRef">
+      <transition
+        :name="`${ns.b()}-fade`"
+        @after-enter="afterEnter"
+        @after-leave="afterLeave"
+        @before-leave="beforeLeave"
+      >
+        <div :class="[`${ns.b()}_content`, anchorClass, 'ltr']" v-show="visible">
+          <div :class="[`${ns.b()}-ink`]">
+            <span :ref="inkNodeRef" />
+          </div>
+          <slot></slot>
+        </div>
+      </transition>
+      <div :class="`${ns.b()}__bar`" @click="visible = !visible">
+        <l-icon :icon="visible ? 'l-youbian' : 'l-zuobian'"></l-icon>
+      </div>
+    </div>
+  </l-sticky>
 </template>
 
 <script lang="ts">
 import { toRefs, ref, h, reactive, onMounted, shallowRef, nextTick, computed } from 'vue';
+import { useEventListener, useThrottleFn } from '@vueuse/core';
 import { useNamespace } from '../../../hooks';
 import createComponent from '../../../utils/create';
 import { anchorProps, anchorEmits } from './anchor';
-const { create } = createComponent('Anchor');
-
+import useProvideAnchor from './context';
 import { Icon } from '../../icon';
 import { Sticky } from '../../sticky';
+import { throwError } from '../../../utils/error';
+import { getScrollContainer } from '../../../utils/dom';
+
+const { create, componentName } = createComponent('Anchor');
+const sharpMatcherRegx = /#([\S ]+)$/;
+type Section = {
+  link: string;
+  top: number;
+};
+type AnchorContainer = HTMLElement | Window | null;
+export interface AnchorState {
+  scrollContainer: AnchorContainer;
+  links: string[];
+  scrollEvent: any;
+  animating: boolean;
+}
 
 export default create({
-    components: {
-        Sticky,
-        Icon
-    },
-    props: anchorProps,
-    emits: anchorEmits,
-    setup(props, { emit, attrs, expose }) {
-        const { affix, wrapperClass, wrapperStyle, showInkInFixed } = props;
+  components: {
+    Sticky,
+    Icon
+  },
+  props: anchorProps,
+  emits: anchorEmits,
+  setup(props, { emit, attrs, expose }) {
+    const { affix, wrapperClass, wrapperStyle, showInkInFixed } = props;
+    const ns = useNamespace('anchor');
+    const root = shallowRef<HTMLDivElement>();
 
-        onMounted(() => {
-            nextTick(() => {
-                if (props.target) {
-                    
-                }
-            })
-        })
+    const el = shallowRef<HTMLElement | undefined>(document.documentElement);
+    const scrollContainer = shallowRef<AnchorContainer>(); // (getScrollContainer(root.value!, true) || document)
+
+    const anchorRef = ref();
+    const inkNodeRef = ref();
+
+    const state = reactive<AnchorState>({
+      links: [],
+      scrollContainer: null,
+      scrollEvent: null,
+      animating: false
+    });
+
+    const activeLink = ref<string | null>(null);
+
+    const visible = ref(true);
+
+    // 固定模式是否显示圆点
+    const anchorClass = { ['']: !affix && !showInkInFixed };
+
+    const afterEnter = () => {};
+    const afterLeave = () => {};
+    const beforeLeave = () => {};
+
+    const getCurrentAnchor = (offset = 0, bounds = 5) => {
+      const linkSections: Array<Section> = [];
+      const container = scrollContainer.value as HTMLElement;
+      state.links.forEach((link) => {
+        const sharpLinkMatch = sharpMatcherRegx.exec(link.toString());
+        if (!sharpLinkMatch) {
+          return;
+        }
+        const target = document.getElementById(sharpLinkMatch[1]);
+        if (target) {
+          const top = getOffsetTop(target, container);
+          if (top < offset + bounds + container.scrollTop) {
+            linkSections.push({
+              link,
+              top
+            });
+          }
+        }
+      });
+
+      if (linkSections.length) {
+        const maxSection = linkSections.reduce((prev, curr) => (curr.top > prev.top ? curr : prev));
+        return maxSection.link;
+      }
+      return '';
+    };
+    function getOffsetTop(element: HTMLElement, container: any): number {
+      if (!element.getClientRects().length) {
+        return 0;
+      }
+      if (!container) {
+        return element.offsetTop;
+      }
+      const rect = element.getBoundingClientRect();
+      const containerRect = container.getBoundingClientRect();
+      // 获取距离父容器的高度
+      const { scrollTop } = container;
+      return rect.top + scrollTop - containerRect.top;
+      // const rect = element.getBoundingClientRect();
+      // if (rect.width || rect.height) {
+      //   if (container === window) {
+      //     container = element.ownerDocument!.documentElement!;
+      //     return rect.top - container.clientTop;
+      //   }
+      //   return rect.top - (container as HTMLElement).getBoundingClientRect().top;
+      // }
+
+      // return rect.top;
     }
-})
+
+    const setCurrentActiveLink = (link: string) => {
+      if (activeLink.value === link) {
+        return;
+      }
+
+      activeLink.value = link;
+      emit('change', link);
+    };
+    const scrollToTop = (link: string) => {
+      const { offsetTop, targetOffset } = props;
+      const container: HTMLElement = scrollContainer.value! as HTMLElement;
+      const sharpLinkMatch = sharpMatcherRegx.exec(link.toString());
+      if (!sharpLinkMatch) {
+        return;
+      }
+      const targetElement = document.getElementById(sharpLinkMatch[1]);
+      if (!targetElement) {
+        return;
+      }
+
+      const eleOffsetTop = getOffsetTop(targetElement, container);
+      let y = eleOffsetTop;
+      y -= targetOffset !== undefined ? targetOffset : offsetTop || 0;
+      const frameFunc = () => {
+        container.scrollTop = y;
+      };
+      requestAnimationFrame(frameFunc);
+    };
+
+    const handleScrollTo = (link: string) => {
+      console.log('handleScrollTo==>', link);
+
+      scrollToTop(link);
+      // 如果区域不可滚动或者滚动行为不是smooth 手动设置当前link
+      const { scrollHeight, clientHeight } = scrollContainer.value as HTMLElement;
+      const scrollBehavior = getComputedStyle(scrollContainer.value as Element).scrollBehavior;
+      if (scrollHeight === clientHeight || scrollBehavior !== 'smooth') {
+        setCurrentActiveLink(link);
+      }
+      // setCurrentActiveLink(link); TODO 待确认不触发主动选中，滚动结束自动选中
+    };
+    expose({
+      scrollTo: handleScrollTo
+    });
+
+    useProvideAnchor({
+      registerLink: (link: string) => {
+        if (!state.links.includes(link)) {
+          console.log('link----', link);
+          state.links.push(link);
+        }
+      },
+      unregisterLink: (link: string) => {
+        const index = state.links.indexOf(link);
+        if (index !== -1) {
+          state.links.splice(index, 1);
+        }
+      },
+      activeLink,
+      scrollTo: handleScrollTo,
+      handleClick: (e, info) => {
+        emit('click', e, info);
+      }
+    });
+    const anchorDefault = computed(() => {
+      return props.defaultAnchor?.charAt(0) !== '#' ? `#${props.defaultAnchor}` : props.defaultAnchor;
+    });
+
+    const handleScroll = () => {
+      if (state.animating) {
+        return;
+      }
+      // 若滚动条滚动到底不更新
+      const { scrollTop, clientHeight, scrollHeight } = scrollContainer.value as HTMLElement;
+      if (scrollTop + clientHeight === scrollHeight && scrollTop > 0) {
+        return;
+      }
+      const { bounds, targetOffset } = props;
+      const currentActiveLink = getCurrentAnchor(targetOffset !== undefined ? targetOffset : 0, bounds);
+      setCurrentActiveLink(currentActiveLink || anchorDefault.value);
+    };
+    const handleScrollThrottled = useThrottleFn(handleScroll, 200);
+
+    onMounted(() => {
+      nextTick(() => {
+        if (props.target) {
+          el.value = document.querySelector<HTMLElement>(props.target) ?? undefined;
+          if (!el.value) {
+            throwError(componentName, `target is not existed: ${props.target}`);
+          }
+          scrollContainer.value = el.value;
+        }
+        if (!scrollContainer.value) {
+          scrollContainer.value = getScrollContainer(root.value!, true);
+        }
+        handleScroll();
+        console.log('scrollContainer.value----', scrollContainer.value, state.links, props);
+      });
+    });
+
+    useEventListener(scrollContainer, 'scroll', handleScrollThrottled);
+
+    return {
+      root,
+      ns,
+      affix,
+      props,
+      wrapperClass,
+      wrapperStyle,
+      anchorRef,
+      inkNodeRef,
+      anchorClass,
+      visible,
+      afterEnter,
+      afterLeave,
+      beforeLeave
+    };
+  }
+});
 </script>
