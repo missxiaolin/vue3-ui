4cc6c87cb6233011f324d3d6dd5ea1f74856e284
462441355@qq.com <462441355@qq.com>
2023-05-19 17:39:36 +0800

    * 优化

--- src/packages/components/page-header/src/page-header.vue
+++ src/packages/components/page-header/src/page-header.vue
@@ -6,16 +6,16 @@
     <div :class="ns.e('header')">
       <div :class="ns.e('left')">
         <div :class="ns.e('back')" role="button" tabindex="0" @click="handleClick">
-          <div v-if="icon" :class="ns.e('icon')"> 
+          <div v-if="icon" :class="ns.e('icon')">
             <icon :icon="icon"></icon>
           </div>
           <div :class="ns.e('title')">
-            <slot name="title">{{title || "返回"}}</slot>
+            <slot name="title">{{ title || '返回' }}</slot>
           </div>
         </div>
         <Divider direction="vertical" />
         <div :class="ns.e('content')">
-          <slot name="content">{{ content || "详情页面" }}</slot>
+          <slot name="content">{{ content || '详情页面' }}</slot>
         </div>
       </div>
     </div>
@@ -28,12 +28,12 @@
 
 <script lang="ts">
 import { PageHeaderProps, pageHeaderEmits } from './page-header';
-import { toRefs, ref, watch, onMounted, computed } from 'vue';
+import { toRefs, ref, watch, onMounted, computed, useSlots } from 'vue';
 import createComponent from '../../../utils/create';
 import { useNamespace } from '../../../hooks';
 const { componentName, create } = createComponent('PageHeader');
 import { Icon } from '../../icon/index';
-import { Divider } from '../../divider/index'
+import { Divider } from '../../divider/index';
 
 export default create({
   components: {
@@ -45,13 +45,20 @@ export default create({
   setup(props, { emit }) {
     const ns = useNamespace('page-header');
     const { icon, title, content } = toRefs(props);
+    const slots = useSlots()
 
     const kls = computed(() => {
-      return [ns.b()];
+      return [
+        ns.b(),
+        {
+          [ns.m('has-breadcrumb')]: !!slots.breadcrumb,
+          [ns.is('contentful')]: !!slots.main
+        }
+      ];
     });
 
     const handleClick = () => {
-      emit('back')
+      emit('back');
     };
 
     return {
