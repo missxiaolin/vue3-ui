eb554680f7519e9377029da7ea97c1e89211a04c
462441355@qq.com <462441355@qq.com>
2023-05-19 14:04:55 +0800

    * 样式

--- src/packages/components/loading-bar/src/loading-bar.vue
+++ src/packages/components/loading-bar/src/loading-bar.vue
@@ -1,11 +1,13 @@
 <template>
   <teleport :to="to ?? 'body'">
     <transition name="l-loading-bar-fade">
-        <div class="l-loading-bar-container">
-            <div ref="loadingBarRef">
-
-            </div>
-        </div>
+      <div class="l-loading-bar-container">
+        <div ref="loadingBarRef" :class="[ns.b(), {
+            'l-loading-bar__starting': isStart,
+            'l-loading-bar__error': isError,
+            'l-loading-bar__finishing': isFinish
+        }]"> </div>
+      </div>
     </transition>
   </teleport>
   <slot></slot>
@@ -26,11 +28,17 @@ export default create({
   setup(props) {
     const ns = useNamespace('loading-bar');
     const { loadingBarStyle, to } = toRefs(props);
+    const isStart = ref(true); // 是否开启
+    const isEnter = ref(false); // 动画是否开启
+    const isError = ref(false); // 是否报错
 
     return {
       componentName,
       ns,
-      to
+      to,
+      isStart,
+      isEnter,
+      isError
     };
   }
 });
