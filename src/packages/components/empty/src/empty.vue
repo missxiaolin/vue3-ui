7027ff51b5c4f369369842e31353504a27d7378a
462441355@qq.com <462441355@qq.com>
2023-05-20 21:17:57 +0800

    * 先这样

---
+++ src/packages/components/empty/src/empty.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        11
+    </div>
+</template>
+
+<script lang="ts">
+import { computed, toRefs } from 'vue';
+import createComponent from '../../../utils/create';
+import { useNamespace } from '../../../hooks';
+
+const { create } = createComponent('Empty');
+
+export default create({
+
+})
+</script>
