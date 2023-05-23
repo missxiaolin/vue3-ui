63b1ec5082d04b04cd141555a2cf79701039ec7a
462441355@qq.com <462441355@qq.com>
2023-05-23 09:58:18 +0800

    * add

---
+++ src/packages/components/collapse/src/collapse.vue
@@ -0,0 +1,22 @@
+<template>
+  <div :class="[ns.b()]" role="tablist" aria-multiselectable="true">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, reactive, toRefs } from 'vue';
+import createComponent from '../../../utils/create';
+import { useNamespace } from '../../../hooks';
+
+const { create } = createComponent('Collapse');
+export default create({
+  setup() {
+    const ns = useNamespace('collapse');
+
+    return {
+      ns
+    };
+  }
+});
+</script>
