5bb4920e7781f257e5a7b8197544d1e5572ee0ae
462441355@qq.com <462441355@qq.com>
2023-05-26 10:16:46 +0800

    * 为了快速生成组件，脚本来做

---
+++ src/packages/components/progress/src/progress.vue
@@ -0,0 +1,22 @@
+<template>
+  <div :class="ns.b()">
+    
+  </div>
+</template>
+
+<script lang="ts">
+import { ref } from 'vue';
+import createComponent from '../../../utils/create';
+import { useNamespace } from '../../../hooks';
+const { create } = createComponent('Progress');
+
+export default create({
+  setup(props, { emit }) {
+    const ns = useNamespace('progress');
+
+    return {
+      ns
+    };
+  }
+});
+</script>
