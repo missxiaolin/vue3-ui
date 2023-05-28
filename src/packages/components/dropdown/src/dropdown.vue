8a9155ce6316175a2dd2e3e37389f7d695e90b49
462441355@qq.com <462441355@qq.com>
2023-05-28 08:13:21 +0800

    * add

---
+++ src/packages/components/dropdown/src/dropdown.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        1
+    </div>
+</template>
+
+<script lang="ts">
+import { computed, CSSProperties, onMounted, onUnmounted, ref } from 'vue';
+import createComponent from '../../../utils/create';
+import { useNamespace } from '../../../hooks';
+const { create, componentName } = createComponent('dropdown');
+
+export default create({
+    setup(props) {
+        
+    }
+})
+
+</script>
