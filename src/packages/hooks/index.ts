ac0f8db31bc450b405c55417191b29abc1f35cd8
462441355@qq.com <462441355@qq.com>
2023-05-28 20:20:35 +0800

    * hooks

--- src/packages/hooks/index.ts
+++ src/packages/hooks/index.ts
@@ -3,3 +3,5 @@ export * from './use-namespace';
 export * from './use-locale';
 export * from './use-forward-ref'
 export * from './use-global-config'
+export * from './use-deprecated'
+export * from './useZIndex'
