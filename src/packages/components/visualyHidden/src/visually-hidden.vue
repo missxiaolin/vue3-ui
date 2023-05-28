2faf886c065006798e309be450f5266296180e81
462441355@qq.com <462441355@qq.com>
2023-05-28 21:02:23 +0800

    * add 简单内部组件

---
+++ src/packages/components/visualyHidden/src/visually-hidden.vue
@@ -0,0 +1,37 @@
+<template>
+  <span v-bind="$attrs" :style="computedStyle"> </span>
+</template>
+
+<script lang="ts">
+import { computed } from 'vue';
+import createComponent from '../../../utils/create';
+const { create } = createComponent('VisuallyHidden');
+export default create({
+  props: {
+    style: {
+      type: [String, Object, Array]
+    }
+  },
+  setup(props) {
+    return {
+      computedStyle: computed(() => {
+        return [
+          props.style,
+          {
+            position: 'absolute',
+            border: 0,
+            width: 1,
+            height: 1,
+            padding: 0,
+            margin: -1,
+            overflow: 'hidden',
+            clip: 'rect(0, 0, 0, 0)',
+            whiteSpace: 'nowrap',
+            wordWrap: 'normal'
+          }
+        ] as any;
+      })
+    };
+  }
+});
+</script>
