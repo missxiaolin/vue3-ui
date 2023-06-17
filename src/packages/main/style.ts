f71aee037dfd4faded48521b6ed5ce43b88af846
462441355@qq.com <462441355@qq.com>
2023-06-17 19:58:35 +0800

    * 导入

--- src/packages/main/style.ts
+++ src/packages/main/style.ts
@@ -49,3 +49,4 @@ import '@/packages/components/notification/style/notification.scss';
 import '@/packages/components/popover/style/popover.scss';
 import '@/packages/components/popconfirm/style/popconfirm.scss';
 import '@/packages/components/skeleton/style/skeleton.scss';
+import '@/packages/components/skeleton/style/skeleton-item.scss';
