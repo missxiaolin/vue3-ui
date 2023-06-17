9c014dd17aec36375c875200edd4330259462ed5
462441355@qq.com <462441355@qq.com>
2023-06-17 21:35:46 +0800

    * add

--- src/packages/hooks/index.ts
+++ src/packages/hooks/index.ts
@@ -16,6 +16,7 @@ export * from './use-prop'
 export * from './use-same-target'
 export * from './use-lockscreen'
 export * from './use-draggable';
+export { default as useThrottleRender } from './use-throttle-render';
 export { default as useRestoreActive } from './use-restore-active';
 export { default as useModal } from './use-modal';
 export { default as usePreventGlobal } from './use-prevent-global';
