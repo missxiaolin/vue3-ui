566e818cff9f43e4acea9df2a7191bf7ee17cce5
462441355@qq.com <462441355@qq.com>
2023-06-21 11:07:09 +0800

    * 头像

--- src/packages/components/avatar/docs/doc.md
+++ src/packages/components/avatar/docs/doc.md
@@ -1,1 +1,54 @@
-## Avatar
+
+# avatar 组件
+Avatar 组件可以用来代表人物或对象， 支持使用图片，图标或者文字作为 Avatar
+
+
+### 基础用法
+使用 shape 和 size 属性来设置 Avatar 的形状和大小。
+
+```vue demo
+<template>
+  <l-row :cols="2" class="demo-avatar demo-basic">
+    <l-col>
+      <div class="demo-avatar-wrapper">
+        <div class="sub-title">circle</div>
+        <div class="demo-basic--circle">
+          <div class="block">
+            <l-avatar :size="50" :src="circleUrl" />
+          </div>
+          <div v-for="size in sizeList" :key="size" class="block">
+            <l-avatar :size="size" :src="circleUrl" />
+          </div>
+        </div>
+      </div>
+    </l-col>
+    <l-col>
+      <div class="demo-avatar-wrapper">
+        <div class="sub-title">square</div>
+        <div class="demo-basic--square">
+          <div class="block">
+            <l-avatar shape="square" :size="50" :src="squareUrl" />
+          </div>
+          <div v-for="size in sizeList" :key="size" class="block">
+            <l-avatar shape="square" :size="size" :src="squareUrl" />
+          </div>
+        </div>
+      </div>
+    </l-col>
+  </l-row>
+</template>
+<script lang="ts" setup>
+import { reactive, toRefs } from 'vue'
+
+const state = reactive({
+  circleUrl:
+    'https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png',
+  squareUrl:
+    'https://cube.elemecdn.com/9/c2/f0ee8a3c7c9638a54940382568c9dpng.png',
+  sizeList: ['small', '', 'large'] as const,
+})
+
+const { circleUrl, squareUrl, sizeList } = toRefs(state)
+</script>
+
+```
--- src/packages/components/avatar/src/avatar.vue
+++ src/packages/components/avatar/src/avatar.vue
@@ -1,24 +1,80 @@
-
 <template>
-<div :class="ns.b()">
-  
-</div>
+  <span :class="avatarClass" :style="sizeStyle">
+    <img
+      v-if="(src || srcSet) && !hasLoadError"
+      :src="src"
+      :alt="alt"
+      :srcset="srcSet"
+      :style="fitStyle"
+      @error="handleError"
+    />
+    <l-icon v-else-if="icon" :size="iconSize" :icon="icon"></l-icon>
+    <slot v-else />
+  </span>
 </template>
 
 <script lang="ts">
-import { ref } from 'vue';
-import createComponent from '../../../utils/create';
+import { computed, ref, watch, toRefs } from 'vue';
+import { Icon } from '../../../components/icon/index';
 import { useNamespace } from '../../../hooks';
-const { create } = createComponent('Avatar');
+import { avatarProps, avatarEmits } from './avatar';
+import createComponent from '../../../utils/create';
+import { addUnit, isNumber, isString } from '../../../utils/util';
+import type { CSSProperties } from 'vue';
+const { create, componentName } = createComponent('Avatar');
 
 export default create({
-setup(props, { emit }) {
-  const ns = useNamespace('avatar');
+  components: {
+    [Icon.name]: Icon
+  },
+  props: avatarProps,
+  emits: avatarEmits,
 
-  return {
-    ns
-  };
-}
+  setup(props, { emit }) {
+    const hasLoadError = ref(false);
+    const ns = useNamespace('avatar');
+    const { name } = toRefs(props);
+
+    const avatarClass = computed(() => {
+      const { size, icon, shape } = props;
+      const classList = [ns.b()];
+      if (isString(size)) classList.push(ns.m(size));
+      if (icon) classList.push(ns.m('icon'));
+      if (shape) classList.push(ns.m(shape));
+      return classList;
+    });
+    const sizeStyle = computed(() => {
+      const { size } = props;
+      return isNumber(size)
+        ? ({
+            '--l-avatar-size': addUnit(size)
+          } as CSSProperties)
+        : undefined;
+    });
+
+    const fitStyle = computed<CSSProperties>(() => ({
+      objectFit: props.fit
+    }));
+    // need reset hasLoadError to false if src changed
+    watch(
+      () => props.src,
+      () => (hasLoadError.value = false)
+    );
+    const iconSize = computed(() => props.size);
+    const handleError = (e: Event) => {
+      hasLoadError.value = true;
+      emit('error', e);
+    };
+    return {
+      ns,
+      name,
+      avatarClass,
+      sizeStyle,
+      fitStyle,
+      handleError,
+      iconSize,
+      hasLoadError
+    };
+  }
 });
 </script>
-
