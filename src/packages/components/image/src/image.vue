fc95d15e1a67fb2b4af1aec8bb602aa01af87525
462441355@qq.com <462441355@qq.com>
2023-06-21 17:20:23 +0800

    * img load

--- src/packages/components/image/src/image.vue
+++ src/packages/components/image/src/image.vue
@@ -44,18 +44,124 @@ export default create({
   props: imageProps,
   emits: imageEmits,
   setup(props, { emit, attrs: rawAttrs }) {
-    const ns = useNamespace('image');
-    const attrs = useAttrs();
+    useDeprecated(
+      {
+        scope: 'l-image',
+        from: 'append-to-body',
+        replacement: 'preview-teleported',
+        version: '0.0.0',
+        ref: ''
+      },
+      computed(() => isBoolean(props.appendToBody))
+    );
+
     const { t } = useLocaleInject();
-    const loading = ref(true);
+    const ns = useNamespace('image');
+
+    const attrs = useAttrs();
     const hasLoadError = ref(false);
+    const loading = ref(true);
+    const imgWidth = ref(0);
+    const imgHeight = ref(0);
+    const showViewer = ref(false);
+    const container = ref<HTMLElement>();
+
+    const _scrollContainer = ref<HTMLElement | Window>();
+    let stopScrollListener: () => void;
+    let stopWheelListener: () => void;
+
+    const imageStyle = computed<CSSProperties>(() => {
+      const { fit } = props;
+      if (isClient && fit) {
+        return { objectFit: fit };
+      }
+      return {};
+    });
 
     const containerStyle = computed(() => rawAttrs.style as StyleValue);
+
+    const loadImage = () => {
+      if (!isClient) return;
+      // reset status
+      loading.value = true;
+      hasLoadError.value = false;
+
+      const img = new Image();
+      const currentImageSrc = props.src;
+
+      // load & error callbacks are only responsible for currentImageSrc
+      img.addEventListener('load', (e) => {
+        if (currentImageSrc !== props.src) {
+          return;
+        }
+        handleLoad(e, img);
+      });
+      img.addEventListener('error', (e) => {
+        if (currentImageSrc !== props.src) {
+          return;
+        }
+        handleError(e);
+      });
+
+      // bind html attrs
+      // so it can behave consistently
+      Object.entries(attrs.value).forEach(([key, value]) => {
+        // avoid onload to be overwritten
+        if (key.toLowerCase() === 'onload') return;
+        img.setAttribute(key, value as string);
+      });
+      img.src = currentImageSrc;
+    }
+
+    function handleLoad(e: Event, img: HTMLImageElement) {
+      imgWidth.value = img.width;
+      imgHeight.value = img.height;
+      loading.value = false;
+      hasLoadError.value = false;
+    }
+
+    function handleError(event: Event) {
+      loading.value = false;
+      hasLoadError.value = true;
+      emit('error', event);
+    }
+
+    async function addLazyLoadListener() {
+
+    }
+
+    function removeLazyLoadListener() {
+
+    }
+
+    watch(
+      () => props.src,
+      () => {
+        if (props.lazy) {
+          // reset status
+          loading.value = true;
+          hasLoadError.value = false;
+          removeLazyLoadListener();
+          addLazyLoadListener();
+        } else {
+          loadImage();
+        }
+      }
+    );
+
+    onMounted(() => {
+      if (props.lazy) {
+        addLazyLoadListener();
+      } else {
+        loadImage();
+      }
+    });
 
     return {
       ns,
       attrs,
       containerStyle,
+      imageStyle,
       t,
       loading,
       hasLoadError
